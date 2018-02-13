49fca4d8cf207bcb84cc52a7bc87d74b3ca0cffd
Liubomyr Kokor <23187063+ljubik@users.noreply.github.com>
2018-02-13 20:15:47 +0200

    20% first tak

--- homework_07/js/task1.js
+++ homework_07/js/task1.js
@@ -1,1 +1,24 @@
 
+var N = parseFloat(prompt("Введіть число N від 0 до 20")); 
+var P = "[~]";
+
+if ( N > 0 && N <= 20 ) {
+	let i = 1;
+	let j = 1;
+		while (i <= N) {
+			
+		    console.log(i);	
+			console.log(P);	
+			++P
+		
+			//alert( i );
+		i++;
+		}
+	//console.log(P + "\n");
+	
+} 
+
+else {
+	console.error("Невірні дані " + N);
+}
+
