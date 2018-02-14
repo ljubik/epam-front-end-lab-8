798beb10632ca5610a19102a1e57b9de898bfd26
Liubomyr Kokor <23187063+ljubik@users.noreply.github.com>
2018-02-14 22:27:14 +0200

    Task #2 - 100%

---
+++ homework_08/js/isSmaller.js
@@ -0,0 +1,9 @@
+var a = Number(prompt("Введіть число a", 1)); 
+var b = Number(prompt("Введіть число b", 5));
+
+function isBigger(a, b)
+{
+var c = a < b;
+alert(a < b);
+}
+console.log(isBigger(a, b));
