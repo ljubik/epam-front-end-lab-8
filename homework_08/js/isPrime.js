7aaf4c2b797c338871be45dfa2c400a19b955dc6
Liubomyr Kokor <23187063+ljubik@users.noreply.github.com>
2018-02-18 14:14:11 +0200

    Task #4 100%

--- homework_08/js/isPrime.js
+++ homework_08/js/isPrime.js
@@ -1,9 +1,10 @@
+var c = Number(prompt("Введіть число", 1));
 function isPrime(value) {
     for(var i = 2; i < value; i++) {
         if(value % i === 0) {
             return false;
         }
-		
     }
     return value > 1;
 }
+console.log(isPrime(c));
