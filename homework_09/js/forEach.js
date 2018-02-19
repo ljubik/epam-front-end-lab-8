bd47073862c2fae70bafd6939fb571e966142b2c
Liubomyr Kokor <23187063+ljubik@users.noreply.github.com>
2018-02-19 11:43:11 +0200

    Task #1 - 80%
    Метод forEach() виконує надану функцію один раз для кожного елемента масиву.
    var arr = ['a', 'b', 'c'];
    arr.forEach(function(element) {
        console.log(element);
    });

--- homework_09/js/forEach.js
+++ homework_09/js/forEach.js
@@ -1,1 +1,11 @@
+var MyArr = [1, 5, 2];
 
+function forEach(MyArr, el){
+	for (var index = 0; index < MyArr.length; ++index) {
+		console.log((MyArr[index]) + " " + el);
+	}
+};
+
+function el(){
+
+};
