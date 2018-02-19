c1d15c49b1e63c1ea8981183403b45a9b6f1b206
Liubomyr Kokor <23187063+ljubik@users.noreply.github.com>
2018-02-19 11:55:47 +0200

    task #1 - 85%

--- homework_09/js/forEach.js
+++ homework_09/js/forEach.js
@@ -2,7 +2,7 @@ var MyArr = [1, 5, 2];
 
 function forEach(MyArr, el){
 	for (var index = 0; index < MyArr.length; ++index) {
-		console.log((MyArr[index]) + " " + el);
+		//console.log((MyArr[index]) + " " + el);
 	}
 };
 
